f806a653cc2bb1e77fe9a22a0b0e7e5ae14929ef
Kurt Lewin <lewin_jota96@yahoo.com>
2025-05-08 01:07:07 +0800

    added url BE

--- backend/models/Business.js
+++ backend/models/Business.js
@@ -4,6 +4,7 @@ const businessSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   img: { type: String, required: true },
+  url: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Business", businessSchema);
